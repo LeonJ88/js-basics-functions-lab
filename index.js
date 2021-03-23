534c69905d2c18fca0c6e0acefa69c58df82e35a
Javier <JavierRLeon@protonmail.com>
2021-03-23 15:18:12 -0400

    commit tree 5cd0d10d2dba75f07459d9debf9457aa48fdb217

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const hQ = 42
+
+function distanceFromHqInBlocks(pickUp) {
+ const blocksAway = hQ > pickUp ? hQ - pickUp : pickUp - hQ;
+ return blocksAway  
+}
+
+function distanceFromHqInFeet(startPoint) {
+    const feetAway = distanceFromHqInBlocks(startPoint) * 264;
+    return feetAway
+}
+
+function distanceTravelledInFeet(start, end) {
+    const feetTraveled = start > end ? start - end : end - start;
+    return feetTraveled * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const customerDistance = distanceTravelledInFeet(start, destination);
+    if (customerDistance <= 400) {
+    return 0;
+    }
+    else if (customerDistance > 400 && customerDistance <= 2000) {
+        return .02 * (customerDistance - 400);
+    }
+    else if (customerDistance > 2000 && customerDistance < 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+
+    
+}
